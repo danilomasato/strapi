b50c8bb9a9a30fed47e3cf8f85ea238e039d22e9
masato koga <masatokoga32@gmail.com>
2026-05-06 23:08:42 -0300

    config plugin nodemailer

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,20 @@
-export default () => ({});
+export default () => ({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('smtp.gmail.com'),
+        port: env.int('SMTP_PORT', 587),
+        secure: false, // Use `true` for port 465
+        auth: {
+          user: env('process.env.GMAIL_ADRESS'),
+          pass: env('process.env.GMAIL_PASSWORD'),
+        },
+      },
+      settings: {
+        defaultFrom: env('process.env.GMAIL_ADRESS'),
+        defaultReplyTo: env('process.env.GMAIL_ADRESS'),
+      },
+    },
+  },
+});
